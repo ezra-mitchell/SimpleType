bc5ee8fe3ece99c850a62ab1763a9f24f03fb362
Mitchell <ezra.mitchell@accenturefederal.com>
2021-12-22 13:30:12 -0700

    project is really initialized this time

---
+++ simpleType/src/main/java/com/afs/ezra/simpleType/config/MvcConfiguration.java
@@ -0,0 +1,34 @@
+package com.afs.ezra.simpleType.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com.afs.ezra.simpleType")
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ViewResolver resolver = new InternalResourceViewResolver("/WEB-INF/jsp/", ".jsp");
+		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		return new CommonsMultipartResolver();
+	}
+}
---
+++ simpleType/src/main/java/com/afs/ezra/simpleType/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.afs.ezra.simpleType.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public ModelAndView homeRedirect() {
+		return new ModelAndView("redirect:/home");
+	}
+	
+	
+	@GetMapping("/home")
+	public ModelAndView home() {
+		return new ModelAndView("home");
+	}
+}
